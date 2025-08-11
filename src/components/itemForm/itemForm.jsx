80fded20ed0ce506bbdeac6fef47e265c4a27f76
Hanoodoo7 <mohalmaskati@gmail.com>
2025-08-11 21:36:45 +0300

    fixed the first part of the form

--- src/components/itemForm/itemForm.jsx
+++ src/components/itemForm/itemForm.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+    import { useState } from "react";
 
-const itemForm = (props) => {
-  const [formData, setFormData] = useState({
-    title: '',
-    text: '',
-    category: '',
-    price: '',
-    condition: '',
-    
-  });
+    const ItemsForm = () => {
+    // State to store all the form input values
+    const [formData, setFormData] = useState({
+        title: "",            
+        description: "",      
+        category: "cars",     
+        price: "",            
+        condition: "ok",      
+        status: "available",  
+        image: ""             
+    });
 
-}
+    }
