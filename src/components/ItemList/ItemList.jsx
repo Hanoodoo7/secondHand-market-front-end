eaff29954a0897c25e17ba262d79205ede22f733
alya <aliya2002mah@gmail.com>
2025-08-12 11:02:16 +0300

    Css testing. some assets added. added classes to some things for css reasons.

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,27 +1,30 @@
 const ItemList = (props) =>{
 
   return (
-  <>
+  <div className="item-list-container">
   <h1>Items List</h1>
-  {props.items.map((item)=>(
-  <Link key={item._id} to={`/item/${item._id}`} >
-    <article>
+  {props.Item.map((Item)=>(
+  <Link key={Item._id} to={`/item/${Item._id}`} >
+    <article className="item-card">
       <header>
-    <h2>{item.title}</h2>
-    <p>
-      {item.seller.username}
-      Date posted: {new Date(item.createdAt).toLocaleDateString()}
-    </p>
-    <p>{item.price}</p>
-    </header>
-    <p>{item.category}</p>
-    <p>{item.description}</p>
-    <p>{item.condition}</p>
-    <p>{item.status}</p>
+    <h2>{Item.title}</h2>
+    <div className="item-meta"></div>
+     <div className="item-meta">
+        <p>By {Item.seller.username}</p>
+        <p>Posted: {new Date(Item.createdAt).toLocaleDateString()}</p>
+      </div>
+     <p className="item-price">{Item.price} BDH</p>
+            </header>
+            <span className="item-category">{Item.category}</span>
+            <p className="item-description">{Item.description}</p>
+            <div className="item-meta">
+              <p>Condition: {Item.condition}</p>
+              <p className="item-status">Status: {Item.status}</p>
+            </div>
     </article>
   </Link>
   ))}
-  </>
+  </div>
 
   )
 
