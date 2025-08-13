ab5033879892ca2773fdcf61ea91daf133fc3ec7
Ali Naser <3linassera7med@gmail.com>
2025-08-13 10:41:27 +0300

    Styling the ItemList

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom"
 const ItemList = (props) =>{
 
   return (
+    <><h1>Items List</h1>
+    <br />
   <div className="item-list-container">
-  <h1>Items List</h1>
   {props.item.map((item)=>(
   <Link key={item._id} to={`/items/${item._id}`} >
     <article className="item-card">
       <header>
     <h2>{item.title}</h2>
-    {/* <div className="item-meta"></div> */}
      <div className="item-meta">
         <p>By {item.seller?.username}</p>
         <p>Posted: {new Date(item.createdAt).toLocaleDateString()}</p>
@@ -18,7 +18,7 @@ const ItemList = (props) =>{
      <p className="item-price">{item.price} BDH</p>
             </header>
             <span className="item-category">{item.category}</span>
-            <p className="item-description">{item.description}</p>
+          
             <div className="item-meta">
               <p>Condition: {item.condition}</p>
               <p className="item-status">Status: {item.status}</p>
@@ -27,7 +27,7 @@ const ItemList = (props) =>{
   </Link>
   ))}
   </div>
-
+</>
   )
 
 }
